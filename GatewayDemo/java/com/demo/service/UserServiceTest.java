cfb67148e324c2fe39f3e040ad17843cc31d56e8
javatechy <deepak.kumar.iet@gmail.com>
2015-12-05 13:54:36 +0530

    Updates UserServiceTest

--- GatewayDemo/java/com/demo/service/UserServiceTest.java
+++ GatewayDemo/java/com/demo/service/UserServiceTest.java
@@ -5,7 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+/**
+ * UserServiceTest.java
+ * @author technocrat
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:spring/application-config.xml", "classpath:spring/mvc-config.xml"})
 public class UserServiceTest {
